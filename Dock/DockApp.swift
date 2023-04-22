7dd68f524d7addd071564dc20f6b42b39b21ab2d
Raul <rauldronk@gmail.com>
2023-04-22 19:35:05 +0300

    Initial Commit

---
+++ Dock/DockApp.swift
@@ -0,0 +1,17 @@
+//
+//  DockApp.swift
+//  Dock
+//
+//  Created by Raul on 22.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct DockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
